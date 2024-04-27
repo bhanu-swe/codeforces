206fb363ce3af396b79894906524d1962bb89fab
bhanu-swe <bhanuvswe@gmail.com>
2024-04-27 17:10:51 +0530

    starting

---
+++ 4A-Watermelon.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n%2==0 && n>2){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    return 0;
+}
